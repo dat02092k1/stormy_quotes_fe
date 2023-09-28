6d02aa47710773bf6c10086017acb155b404361f
dat02092k1 <ngodat02092001@gmail.com>
2023-09-28 23:26:56 +0700

    "done config fucking tailwind"

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "bg_img": "bg-[url('https://static2.vieon.vn/voting/static/media/bar-vote-list.81f7d35d2e63a96467ef.png')]"
+      }
+    },
+  },
+  plugins: [],
+}
